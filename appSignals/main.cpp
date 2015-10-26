4df43fd42f06064ee6b9eda6b58ebb42eb3126b7
Cesar Bustamante <cesarbb2007@gmail.com>
2015-10-26 00:34:15 -0300

    prueba

--- appSignals/main.cpp
+++ appSignals/main.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSplashScreen>
+#include <QThread>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QSplashScreen *splash = new QSplashScreen(QPixmap("ucm.png"),Qt::WindowStaysOnTopHint | Qt::SplashScreen);
+    splash->show();
+    //splash->showMessage("Iniciando ventana principal ...");
+
+    w.upOctave();
+    //splash->showMessage("Cargando modulos ...", Qt::TopRightSection, Qt::white);
+    //splash->showMessage("Estableciendo conexiones ...", Qt::AlignRight | Qt::AlignTop,Qt::white);
+    QThread::sleep(5);
+
     w.show();
+    splash->finish(&w);
+    delete splash;
+
 
     return a.exec();
 }
--- appSignals/mainwindow.h
+++ appSignals/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QProcess>
+#include "qcustomplot.h"
+
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +18,26 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    struct Pto{ double x,y; };
+    void upOctave();
+
+
+private slots:
+    void on_botonCargar_clicked();
+    void openOctave(QString opcion);
+    void grafica(QCustomPlot *grafico, QVector<double> x, QVector<double> y );
+    void habilita();
+    int leer(QFile &fin, QList<Pto> & lista);
+    void on_botonGraficar_clicked();
+    void showState();
+    void changeType(QStringList q, QVector<double> & vector,int size);
 
 private:
     Ui::MainWindow *ui;
+    QString path;
+    QProcess *octave;
+
+
 };
 
 #endif // MAINWINDOW_H
