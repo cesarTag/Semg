b559e52c835388e385e2fac8f3da3c0c0f77ea9d
diego campos <d_campos@hotmail.cl>
2015-10-26 11:57:13 -0300

    subiendo archivos con el mismo nombre otra vez

--- Semgs/main.cpp
+++ Semgs/main.cpp
@@ -1,10 +1,10 @@
-#include "mainwindow.h"
+#include "emg.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    EMG w;
     w.show();
 
     return a.exec();
